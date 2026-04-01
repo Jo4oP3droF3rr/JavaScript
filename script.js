6f93f76433a0ad3596862317a575696b10ddea1e
Joao <joao.fpinheiro@outlook.com>
2026-04-01 20:45:36 -0300

    asde

--- script.js
+++ script.js
@@ -8,3 +8,4 @@ const api = "CHAVE_DE_API";
 confirm('Você gosta do ' + nome + '?');
 console.log(1/0) // <- vai dar Infinity, número inexistente ou muito grande
 console.log(1/"a") // <- via dar NaN
+console.log(typeof api)
