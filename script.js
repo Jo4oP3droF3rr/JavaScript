14ac22c58e3f81cbb0a9a1e3fcf97e8e8a65c805
Joao <joao.fpinheiro@outlook.com>
2026-04-01 20:52:51 -0300

    asdsa

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 // confirm("Você deseja sair?");
 //comentário, Ctrl + ; VsCode comenta automaticamente.
 
-let nome = "joao pedro";
+let nome = prompt('Qual seu nome?:');
 const api = "CHAVE_DE_API";
 confirm('Você gosta do ' + nome + '?');
+confirm(`seu nome é ${nome}`) //<- ATENÇÃO: não está entre aspas, está entre CRÁSE!
 console.log(1/0) // <- vai dar Infinity, número inexistente ou muito grande
 console.log(1/"a") // <- via dar NaN
 console.log(typeof api)
